7632e12f6f278e9b812a415fb2ed2d05e76d6c0b
yzd <yzd@yzdeng.xyz>
2021-12-14 22:54:51 +0800

    some other lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -105,7 +105,7 @@ return require('packer').startup(function(use)
   local capabilities = vim.lsp.protocol.make_client_capabilities()
   capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
   -- Enable some language servers with the additional completion capabilities offered by nvim-cmp
-  local servers = { 'ccls', 'tsserver', 'pyright' }
+  local servers = { 'ccls', 'tsserver', 'pyright', 'bashls', 'gopls' }
 
   for _, lsp in ipairs(servers) do
     nvim_lsp[lsp].setup {
@@ -113,6 +113,37 @@ return require('packer').startup(function(use)
       capabilities = capabilities,
     }
   end
+
+  -- Lua language server
+  local runtime_path = vim.split(package.path, ';')
+  table.insert(runtime_path, "lua/?.lua")
+  table.insert(runtime_path, "lua/?/init.lua")
+
+  require'lspconfig'.sumneko_lua.setup {
+    cmd = { 'lua-language-server' };
+    settings = {
+      Lua = {
+        runtime = {
+          -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+          version = 'LuaJIT',
+          -- Setup your lua path
+          path = runtime_path,
+        },
+        diagnostics = {
+          -- Get the language server to recognize the `vim` global
+          globals = {'vim'},
+        },
+        workspace = {
+          -- Make the server aware of Neovim runtime files
+          library = vim.api.nvim_get_runtime_file("", true),
+        },
+        -- Do not send telemetry data containing a randomized but unique identifier
+        telemetry = {
+          enable = false,
+        },
+      },
+    },
+  }
 
   -- Set completeopt to have a better completion experience
   vim.o.completeopt = 'menuone,noselect'
@@ -181,4 +212,3 @@ return require('packer').startup(function(use)
     require('packer').sync()
   end
 end)
-
