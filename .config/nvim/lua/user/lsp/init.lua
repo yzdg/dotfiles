d53511cc6e2702f4c463cfd4bcb137c9485ffc9f
yzd <me@yzdeng.xyz>
2022-01-19 19:15:12 +0800

    nvim: some keymaps

--- .config/nvim/lua/user/lsp/init.lua
+++ .config/nvim/lua/user/lsp/init.lua
@@ -34,7 +34,7 @@ local function lsp_keymaps(bufnr)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "gl", '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "]d", '<cmd>lua vim.diagnostic.goto_next({ border = "rounded" })<CR>', opts)
   vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", opts)
-  vim.cmd [[ command! Format execute 'lua vim.lsp.buf.formatting()' ]]
+  vim.api.nvim_buf_set_keymap(bufnr, "n", "<leader>f", "<cmd>lua vim.lsp.buf.formatting()<CR>", opts)
 end
 
 local on_attach = function(client, bufnr)
