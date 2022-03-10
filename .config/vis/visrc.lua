c35420ba72c39353cc62a692a9ce5dc69dd49cc1
yzd <me@yzdeng.xyz>
2022-03-10 09:39:30 +0800

    vis

---
+++ .config/vis/visrc.lua
@@ -0,0 +1,17 @@
+-- load standard vis module, providing parts of the Lua API
+require('vis')
+
+vis.events.subscribe(vis.events.INIT, function()
+	-- global configuration options
+end)
+
+vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+	-- per window configuration options
+	vis:command('set tw 2')
+	vis:command('set ai')
+	vis:command('set et')
+	vis:command('set nu')
+	vis:command('set cul')
+	vis:command('set cc 80')
+	vis:command('set theme default-256')
+end)
