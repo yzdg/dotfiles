d0126cc9e76fb75f062821edb7d38c5a4a6485fc
yzd <me@yzdeng.xyz>
2022-03-15 19:15:00 +0800

    vis: editorconfig

--- .config/vis/visrc.lua
+++ .config/vis/visrc.lua
@@ -1,5 +1,6 @@
 -- load standard vis module, providing parts of the Lua API
 require('vis')
+require('edconf')
 
 vis.events.subscribe(vis.events.INIT, function()
 	-- global configuration options
@@ -9,7 +10,6 @@ vis.events.subscribe(vis.events.WIN_OPEN, function(win)
 	-- per window configuration options
 	vis:command('set tw 2')
 	vis:command('set ai')
-	vis:command('set et')
 	vis:command('set nu')
 	vis:command('set cul')
 	vis:command('set cc 80')
