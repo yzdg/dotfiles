a6fd1d6334bf7870c484a8b3ed4cb4a1970a02d3
yzd <me@yzdeng.xyz>
2022-03-25 10:50:48 +0800

    emacs: simple evil keymaps

--- .config/emacs/lisp/init-keymap.el
+++ .config/emacs/lisp/init-keymap.el
@@ -3,4 +3,10 @@
 (global-set-key (kbd "C-c i") 'open-init-file)
 (global-set-key (kbd "C-c d") 'delete-trailing-whitespace)
 
+;; evil
+(evil-global-set-key 'normal (kbd "<SPC> -") 'evil-window-split)
+(evil-global-set-key 'normal (kbd "<SPC> =") 'evil-window-vsplit)
+(evil-global-set-key 'normal (kbd "u") 'undo-fu-only-undo)
+(evil-global-set-key 'normal (kbd "C-r") 'undo-fu-only-redo)
+
 (provide 'init-keymap)
